8a95d41f38303b2e4144248be490a4896242f21a
sagor-khan7 <sksagor791@gmail.com>
2025-07-09 21:28:32 +0600

    created blogs components and fetch blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Blogs from "./components/Blogs/Blogs";
 import Header from "./components/Header/Header";
 
 export default function App() {
   return (
     <main className="container mx-auto">
       <Header />
+      <Blogs />
     </main>
   );
 }
